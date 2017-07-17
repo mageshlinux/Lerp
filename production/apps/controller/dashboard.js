d945d5326975e9aaa7fbaf9f0760e2f00bef3a8d
deepak <gdeepak8822@gmail.com>
2017-07-17 12:40:08 +0530

    dashboard js added

--- production/apps/controller/dashboard.js
+++ production/apps/controller/dashboard.js
@@ -8,8 +8,32 @@ $(document).ready(function() {
     $("#Logspan").text(UserData.USERNAME);
 });
 
-function CallForm()
-{
-    $("#MainContainer").load("apps/view/sample.html");
-    return false;
+function CallForm() {
+    $("#MainContainer").load("apps/view/sample.html", function (responseText, textStatus, XMLHttpRequest) {
+        $('.collapse-link').on('click', function() {
+            var $BOX_PANEL = $(this).closest('.x_panel'),
+                $ICON = $(this).find('i'),
+                $BOX_CONTENT = $BOX_PANEL.find('.x_content');
+
+            // fix for some div with hardcoded fix class
+            if ($BOX_PANEL.attr('style')) {
+                $BOX_CONTENT.slideToggle(200, function(){
+                    $BOX_PANEL.removeAttr('style');
+                });
+            } else {
+                $BOX_CONTENT.slideToggle(200);
+                $BOX_PANEL.css('height', 'auto');
+            }
+
+            $ICON.toggleClass('fa-chevron-up fa-chevron-down');
+        });
+
+        $('.close-link').click(function () {
+            var $BOX_PANEL = $(this).closest('.x_panel');
+
+            $BOX_PANEL.remove();
+        });
+
+
+    });
 }
