e03fb2f375c774c5536bbbbe8a0ac923a08820f9
deepak <gdeepak8822@gmail.com>
2017-07-19 13:11:37 +0530

    Client html

--- production/apps/API/Client/InsertClient.php
+++ production/apps/API/Client/InsertClient.php
@@ -1,7 +1,58 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Deepak
- * Date: 7/19/2017
- * Time: 12:43 PM
- */
+
+require_once '../../../../dbconfig.php';
+
+if($_POST)
+{
+    $clientname = $_POST['clientname'];
+    $clientphno = $_POST['clientphno'];
+    $clientaddress = $_POST['clientaddress'];
+    $clientcasetype = $_POST['clientcasetype'];
+    $clienttotalamount = $_POST['clienttotalamount'];
+    $clientpaidamount = $_POST['clientpaidamount'];
+    $clientbalamount = $_POST['clientbalamount'];
+    $clientcasestage = $_POST['clientcasestage'];
+    $clienthearingdate = $_POST['clienthearingdate'];
+
+    try
+    {
+
+        $stmt = $db_con->prepare("select * from client where clientname=:clientname");
+        $stmt->execute(array(":clientname"=>$clientname));
+        $count = $stmt->rowCount();
+
+        if($count==0){
+
+            $stmt = $db_con->prepare("INSERT INTO client (CLIENTNAME, PHNO, ADDRESS, CASETYPE, ATTACHMENT, TOTALAMOUNT, PAIDAMOUNT, BALANCEAMOUNT, CASESTAGE, HEARINGDATE) VALUES(:clientname, :clientphno, :clientaddress, :clientcasetype, 0x44454D4F, :clienttotalamount, :clientpaidamount, :clientbalamount, :clientcasestage, :clienthearingdate)");
+            $stmt->bindParam(":clientname",$clientname);
+            $stmt->bindParam(":clientphno",$clientphno);
+            $stmt->bindParam(":clientaddress",$clientaddress);
+            $stmt->bindParam(":clientcasetype",$clientcasetype);
+            $stmt->bindParam(":clienttotalamount",$clienttotalamount);
+            $stmt->bindParam(":clientpaidamount",$clientpaidamount);
+            $stmt->bindParam(":clientbalamount",$clientbalamount);
+            $stmt->bindParam(":clientcasestage",$clientcasestage);
+            $stmt->bindParam(":clienthearingdate",$clienthearingdate);
+
+            if($stmt->execute())
+            {
+                echo "registered";
+            }
+            else
+            {
+                echo "Query could not execute !";
+            }
+
+        }
+        else{
+
+            echo "1"; //  not available
+        }
+
+    }
+    catch(PDOException $e){
+        echo $e->getMessage();
+    }
+}
+
+?>
--- production/apps/controller/Client.js
+++ production/apps/controller/Client.js
@@ -3,7 +3,7 @@
  */
 $(document).ready(function() {
     $('#ClientForm').validator();
-    $('#client-hearingdate').daterangepicker({
+    $('#clienthearingdate').daterangepicker({
         singleDatePicker: true,
         startDate: moment(),
         locale:{format: 'DD/MMM/YYYY'}
@@ -13,4 +13,37 @@ function SaveClient()
 {
     var data = $("#ClientForm").serialize();
     alert(data)
+    NProgress.start();
+    $.ajax({
+
+        type : 'POST',
+        url  : 'apps/API/Client/InsertClient.php',
+        data : data,
+        beforeSend: function()
+        {
+
+        },
+        success :  function(data)
+        {
+
+            if(data==1){
+                NProgress.done();
+                $.notify("Client Name Already taken.Please Try different Client Name","info",{autoHide:false});
+
+            }
+            else if(data=="registered")
+            {
+                NProgress.done();
+                $.notify("New Client Created Successfully","success");
+                $('#ClientForm').get(0).reset();
+
+            }
+            else{
+                NProgress.done();
+                $.notify("Server encountered error.Please check with Administrator","error");
+
+            }
+        }
+    });
+    return false;
 }
