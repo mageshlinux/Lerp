b4807bb42895cfc4905c8ceac86309b1ff4e878d
deepak <gdeepak8822@gmail.com>
2017-08-18 09:39:10 +0530

    Mailer added

---
+++ vendors/Mailer/mail.php
@@ -0,0 +1,40 @@
+<?php
+require 'PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//Enable SMTP debugging. 
+//$mail->SMTPDebug = 3;                               
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();            
+//Set SMTP host name                          
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                          
+//Provide username and password     
+$mail->Username = "lawerp17@gmail.com";                 
+$mail->Password = "fusion123";                           
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                           
+//Set TCP port to connect to 
+$mail->Port = 587;                                   
+
+$mail->From = "lawerp17@gmail.com";
+$mail->FromName = "Law";
+
+$mail->addAddress("gdeepak8822@gmail.com", "Test");
+
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+if(!$mail->send()) 
+{
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} 
+else 
+{
+    echo "Message has been sent successfully";
+}
