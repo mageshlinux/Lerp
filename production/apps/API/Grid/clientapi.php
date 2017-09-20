731c5d5616b2780a00f25b9386dc67f40a283b2a
deepak <gdeepak8822@gmail.com>
2017-09-20 14:45:18 +0530

    common client pick added

--- production/apps/API/Grid/clientapi.php
+++ production/apps/API/Grid/clientapi.php
@@ -5,3 +5,24 @@
  * Date: 9/20/2017
  * Time: 2:40 PM
  */
+require_once '../../../../dbconfig.php';
+
+if($_POST) {
+    $Query = $_POST['Query'];
+
+    $q = $db_con->prepare($Query);
+    $q->execute(array());
+
+    if ($q->rowCount() > 0){
+        $check = $q->fetchAll(PDO::FETCH_ASSOC);
+        //$row_id = $check['id'];
+        // do something
+        echo json_encode($check);
+    }
+    else
+    {
+        echo 0;
+    }
+
+}
+?>
